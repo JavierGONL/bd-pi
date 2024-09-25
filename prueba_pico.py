24e6214caefa0f99058ee70469506917b40f906f
JavierGONL <JavierGONL@users.noreply.github.com>
2024-09-25 10:18:09 -0500

    Delete prueba_pico.py

--- prueba_pico.py
+++
@@ -1,8 +0,0 @@
-import machine 
-import utime  
-led_externo = machine.Pin('LED', machine.Pin.OUT) 
-
-while True:     
-   led_externo.value(0)         
-   utime.sleep(2)     
-   led_externo.value(1) 
