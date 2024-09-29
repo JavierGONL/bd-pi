ea703cb36b5e40c94541b858dae6560a15d4554d
David-A-Montes-R <158982203+David-A-Montes-R@users.noreply.github.com>
2024-09-29 13:29:59 -0500

    Update boot.py

--- pruebas programacion/boot.py
+++ pruebas programacion/boot.py
@@ -1,4 +1,5 @@
 #boot.py
+#ignorar este archivo, la función de boot está incluida en main.py
 #primero se ejecuta el boot para conectarlo a wifi
 import network
 import time
@@ -12,7 +13,3 @@ while not wlan.isconnected() and wlan.status() >= 0:
         break
 print(wlan.ifconfig())
 time.sleep(5)
-#instale boot.py si desea poder reconfigurar la pi pico w posteriormente
-#si solo instala main.py, la pico posteriormente no se dejará conectar al pc
-#en dado caso, la solución es formatear la memoria de la pi pico w
-#las instrucciones para esto se encuentran en la página oficial de raspberry pi
